291f0590e5b95f5b61efce77c479db69dabb87b9
Saiket Das <saiketdas02@gmail.com>
2022-07-24 16:16:48 +0800

    Delivery and Pickup Button

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function HeaderTabs() {
     return (
-        <View>
-            <Text>HeaderTabs</Text>
+        <View style={styles.tabContainer}>
+            {/* ----- Header Tabs Button ----- */}
+            <HeaderButton
+                text='Delivery' btnColor='black' textColor='white'
+            />
+
+            <HeaderButton
+                text='Pickup' btnColor='white' textColor='black'
+            />
         </View>
-    )
-}
+    );
+};
+
+
+const HeaderButton = (props) => (
+    <TouchableOpacity style={{
+        ...styles.tabs, backgroundColor: props.btnColor,
+    }}>
+        <Text style={{
+            ...styles.tabText, color: props.textColor
+        }}>
+            {props.text}
+        </Text>
+    </TouchableOpacity>
+)
+
+
+const styles = StyleSheet.create({
+    tabContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    tabs: {
+        paddingVertical: 6,
+        paddingHorizontal: 15,
+        borderRadius: 20,
+    },
+    tabText: {
+        fontSize: 15,
+        fontWeight: '700',
+    },
+});
+
